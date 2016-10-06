de6318fdddad4af1685c2292c0d52bbe88874c37
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-10-06 22:39:48 +0000

    Closes #3, Closes #1 using literals

--- bc/lexer.py
+++ bc/lexer.py
@@ -1,22 +1,23 @@
 from ply import lex
 
 tokens = (
-        'PLUS'  ,
-        'MINUS' ,
-        'TIMES' ,
-        'DIVIDE',
-        'POWER' ,
+        'HELP',
+        'LOAD',
+        'SAVE',
+        'SHOW',
+        'SHOW_ALL'
         )
 
-t_PLUS   = r'\+'
-t_MINUS  = r'-'
-t_TIMES  = r'\*'
-t_DIVIDE = r'/'
-t_POWER  = r'\^'
+literals = [ '+','-','/','*', '^']
+
+t_HELP     = r'help'
+t_LOAD     = r'load'
+t_SAVE     = r'save'
+t_SHOW     = r'show'
+t_SHOW_ALL = r'show_all'
 
 def t_error(token):
-    # token.lexer.skip(1)
-
+    token.lexer.skip(1)
     return ('ILLEGAL CHAR', token.value, token.lineno, token.lexpos)
 
 lexer = lex.lex()
