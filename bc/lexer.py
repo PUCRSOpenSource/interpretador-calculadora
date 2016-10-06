fa4756466031a20f1fbb0c53362882031007a2c4
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-10-06 01:30:19 +0000

    This closes #1 and #5

--- bc/lexer.py
+++ bc/lexer.py
@@ -1,6 +1,22 @@
 from ply import lex
 
-tokens = {
-        }
+tokens = (
+        'PLUS'  ,
+        'MINUS' ,
+        'TIMES' ,
+        'DIVIDE',
+        'POWER' ,
+        )
+
+t_PLUS   = r'\+'
+t_MINUS  = r'-'
+t_TIMES  = r'\*'
+t_DIVIDE = r'/'
+t_POWER  = r'\^'
+
+def t_error(token):
+    # token.lexer.skip(1)
+
+    return ('ILLEGAL CHAR', token.value, token.lineno, token.lexpos)
 
 lexer = lex.lex()
