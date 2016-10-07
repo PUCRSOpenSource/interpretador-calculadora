585c22bdfefc85c1dfe289301d8f191bb4cdddf7
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2016-10-07 02:44:56 +0000

    Reconhece algumas palavras reservadas #4, closes #3, closes #2

--- bc/lexer.py
+++ bc/lexer.py
@@ -1,23 +1,49 @@
-from ply import lex
+import ply.lex as lex
 
-tokens = (
-        'HELP',
-        'LOAD',
-        'SAVE',
-        'SHOW',
-        'SHOW_ALL'
-        )
+class BcLexer():
+    def __init__(self):
+        self.reserved = {
+                'help'     : 'HELP',
+                'load'     : 'LOAD',
+                'save'     : 'SAVE',
+                'show'     : 'SHOW',
+                'show_all' : 'SHOW_ALL',
+                'if'       : 'IF',
+                'while'    : 'WHILE',
+                'for'      : 'FOR',
+                }
 
-literals = ['+','-','/','*', '^', '<', '>', '=', '!', '&', '|', ':', '?']
+        self.tokens = ['ID'] + list(self.reserved.values())
+        self.literals = ['+', '-', '/', '*', '^', '<', '>', '=', '!', '&', '|', ':', '?']
+        self.t_ignore  = ' \t\r'
 
-t_HELP     = r'help'
-t_LOAD     = r'load'
-t_SAVE     = r'save'
-t_SHOW     = r'show'
-t_SHOW_ALL = r'show_all'
+    def t_newline(self, token):
+        r'\n+'
+        token.lexer.lineno += 1
 
-def t_error(token):
-    token.lexer.skip(1)
-    return ('ILLEGAL CHAR', token.value, token.lineno, token.lexpos)
+    def find_column(self, input,token):
+        last_cr = input.rfind('\n',0,token.lexpos)
+        if last_cr < 0:
+            last_cr = 0
+        column = (token.lexpos - last_cr) + 1
+        return column
 
-lexer = lex.lex()
+    def t_ID(self, token):
+        r'[a-z-A-Z_][a-zA-Z_0-9]*'
+        token.type = self.reserved.get(token.value, 'ID')
+        return token
+
+    def t_error(self, token):
+        token.lexer.skip(1)
+        return ('ILLEGAL CHAR', token.value, token.lineno, token.lexpos)
+
+    def parse(self, data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            print(tok)
+
+    def build(self):
+        self.lexer = lex.lex(module=self)
