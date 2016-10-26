b7dd2bc7be30cb043edfca6ad46b342441dea64f
Diego Jornada <diego.p.jornada@gmail.com>
2016-10-26 15:19:31 -0200

    Update lexer.py

--- bc/lexer.py
+++ bc/lexer.py
@@ -11,39 +11,51 @@ class BcLexer():
                 'if'       : 'IF',
                 'while'    : 'WHILE',
                 'for'      : 'FOR',
+                'define'   : 'DEFINE'
                 }
 
         self.tokens = ['ID'] + list(self.reserved.values())
-        self.literals = ['+', '-', '/', '*', '^', '<', '>', '=', '!', '&', '|', ':', '?']
+        self.literals = ['+', '-', '/', '*', '^', '<', '>', '=', 
+                         '!', '&', '|', ':', '?', '(', ')', '{', 
+                         '}', '[', ']', ';']
+        
         self.t_ignore  = ' \t\r'
 
     def t_newline(self, token):
         r'\n+'
         token.lexer.lineno += 1
 
-    def find_column(self, input,token):
+    def t_id(self, token):
+        r'[a-z-A-Z_][a-zA-Z_0-9]*'
+        token.type = self.reserved.get(token.value, 'ID')
+        return token
+    
+    def t_number(self, token): 
+        r'\d+'
+        try: 
+            token.value = int(token.value)
+        except ValueError: 
+            print("Integer value too large %d", token.value) token.value = 0 
+        return token
+    
+    def t_error(self, token):
+        token.lexer.skip(1)
+        return ('ILLEGAL', token.value, token.lineno, token.lexpos)
+        
+    def find_column(self, input, token):
         last_cr = input.rfind('\n',0,token.lexpos)
         if last_cr < 0:
             last_cr = 0
         column = (token.lexpos - last_cr) + 1
         return column
 
-    def t_ID(self, token):
-        r'[a-z-A-Z_][a-zA-Z_0-9]*'
-        token.type = self.reserved.get(token.value, 'ID')
-        return token
-
-    def t_error(self, token):
-        token.lexer.skip(1)
-        return ('ILLEGAL CHAR', token.value, token.lineno, token.lexpos)
-
     def parse(self, data):
         self.lexer.input(data)
         while True:
-            tok = self.lexer.token()
-            if not tok:
+            token = self.lexer.token()
+            if not token:
                 break
-            print(tok)
-
+            print(token)
+                       
     def build(self):
         self.lexer = lex.lex(module=self)
