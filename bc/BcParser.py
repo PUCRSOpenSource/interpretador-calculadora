87bccbe0dd50f0316cd4dc7a2b1ffa8d56d0ad6e
marinalb <marina.barros@acad.pucrs.br>
2016-10-30 21:04:46 -0200

    Update BcParser.py
    Adicionado While

--- bc/BcParser.py
+++ bc/BcParser.py
@@ -53,6 +53,7 @@ class BcParser():
         """
             statement  : IF LPAREN expr RPAREN statement
                        | IF LPAREN expr RPAREN statement ELSE statement
+                       | P_WHILE_LOOP LPAREN expr RPAREN statement 
         """
 
     def p_expr_bin(self, token):
